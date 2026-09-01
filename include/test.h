7d18b6dfe664a3b655e7eed4cc94cd7591506ce1
_ov4 <ov4@ov4.org>
2026-09-01 02:48:34 +0000

    fix

--- include/test.h
+++ include/test.h
@@ -2,8 +2,5 @@
 
 namespace ov4
 {
-    void test(char **argv)
-    {
-        return;
-    }
+    void test(char **argv);
 }
---
+++ src/test.cc
@@ -0,0 +1,13 @@
+#include "test.h"
+
+namespace ov4
+{
+
+using namespace std;
+
+void test(char **argv)
+{
+    return;
+}
+
+}
