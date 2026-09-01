c1733952f1bbb567b335109f51fb66a73abfedd3
_ov4 <ov4@ov4.org>
2026-09-01 06:35:52 +0000

    path .

--- include/path.h
+++ include/path.h
@@ -5,12 +5,17 @@
 #include <string>
 #include <unistd.h>
 #include <iterator>
+#include <iostream>
 
 
 namespace ov4
 {
 
+extern std::string current_dir;
+
 std::string find_cmd(char* s);
 std::string find_cmd(std::string s);
+std::string set_current_dir(std::string s);
+std::string path_unifier(const std::string &s);
 
 }
--- src/path.cc
+++ src/path.cc
@@ -5,6 +5,8 @@ namespace ov4
 
 using namespace std;
 
+string current_dir;
+
 string find_cmd(char* s)
 {
     string tmp = s;
@@ -13,8 +15,11 @@ string find_cmd(char* s)
 
 string find_cmd(string s)
 {
-    if (s.size() == 0) return "/";
-    if (s[0] == '/') return s;
+    if (s.size() == 0) return "";
+    string unified_s = path_unifier(s);
+    cout << unified_s << endl;
+    if (access(unified_s.c_str(), F_OK) == 0)
+            return unified_s;
 
     string path = getenv("PATH");
     size_t i = 0, j = 0, l = path.size();
@@ -35,6 +40,21 @@ string find_cmd(string s)
         i=j;
 
     }
+}
+
+string path_unifier(const string &s)
+{
+    if (s.size() == 0) return "";
+    if (s[0] == '/') return s;
+    if (s[0] == '.')
+    {
+        return current_dir + s.substr(1);
+    }
+}
+
+string set_current_dir(string s)
+{
+    return current_dir = s;
 }
 
 
